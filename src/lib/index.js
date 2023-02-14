d17ed9de91a48131979a669b57c1ee24d371a878
Jakub Bystroński <jbstr85@gmail.com>
2023-02-14 01:58:09 +0100

    fix: change wrong exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,7 @@
-export { VirtualGrid as Grid } from './utils/VirtualGrid';
-export { VirtualList as List } from './utils/VirtualList';
-export { VirtualTree as Tree } from './utils/VirtualTree';
+import Grid__SvelteComponent_ from './components/Grid.svelte';
+import Tree__SvelteComponent_ from './components/Tree.svelte';
+import List__SvelteComponent_ from './components/List.svelte';
+
+export { Grid__SvelteComponent_ as Grid };
+export { Tree__SvelteComponent_ as Tree };
+export { List__SvelteComponent_ as List };
