a12032b7f354503d7409a40612c6dd3bb1b7304f
Jakub Bystroński <jbstr85@gmail.com>
2023-02-14 01:25:27 +0100

    fix: fix exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,3 @@
-// Reexport your entry components here
-import { VirtualGrid } from './utils/VirtualGrid';
-import { VirtualList } from './utils/VirtualList';
 export { VirtualGrid as Grid } from './utils/VirtualGrid';
 export { VirtualList as List } from './utils/VirtualList';
 export { VirtualTree as Tree } from './utils/VirtualTree';
